bf07c198442fd366ded12fbf97db185b2b9f43bc
Sabri Ünal <libreajans@gmail.com>
2021-12-28 20:10:30 +0300

    Biraz denetim ekleyelim.
    Biraz daha denetim ekledik. Artık sadece belirtilen değer türü varsa değer dönüyoruz.
    
    İlaveten ana döngüyü kırma özelliği de eklendi. Böylece bir eşleşme bulduysa ikincil eşleşme için bir arama yapmıyor.

--- Route.php
+++ Route.php
@@ -1,32 +1,42 @@
 <?php
 
-	namespace Yakusha;
+	namespace Yakusha\Kitaphaber;
 	
-	/***
-	* Berbat belgelendirme için beni bağışlayın lütfen.
-	* 
+	/**
 	* @page 	: İşlem sonunda hangi sayfanın include edilmesi gerektiğini depolamakta kullanıyoruz
+	* 
 	* @pattern	: Kullanacağımız eşleştirme regex patternlerini burada tanımlıyoruz
 	* 			  Aynı sayfayı birden fazla dış url'den çözümleyebilmek için burada 
 	*			  dizi kullanıyoruz. Her pattern anahtarsız bir dizi olarak tanımlanmalı
+	*			  
+	* @match	: Eşleyen kaçıncı değerin alınacağını gösterir, bunlar 0,1,2 şeklinde bir sıralamayla gider
+	* 			  
+	* @type		: Elde etmek istediğimiz sonucun tipini belirlemeye yarar, farklı bir değer gelmişse router yoluna devam eder
+	* 			  kabul ettiği değerler (numeric, string)
+	* 
 	* @param	: Yeni sayfaya aktarılmasını istediğimiz parametreleri burada dizi olarak 
-	* 			  oluşrup gönderiyoruz. daha sonra extract komutu ile değişkene dönüştürebiliyoruz
+	* 			  oluşturup gönderiyoruz. daha sonra extract komutu ile değişkene dönüştürebiliyoruz
+	* 			  
 	*
-	* Sonuç kısmında sonucu bir dizi olarak dönüyouz, dizi içinde iki eşleşmiş sonuç olabiliyor
-	* İşimize yarayan sonucu duruma göre kullanıyoruz.			 
+	* @return	: Sonuç kısmında eşleşmiş değerleri ve match değerini dönüyoruz.
 	*/
 	
 
 	class Route
 	{
+	
+		public function __construct(){}
+	
 		private static $routes = Array();
 
-		public static function add($page, $pattern = array(), $param = array())
+		public static function add($page, $pattern = array(), $match = 2, $type = 'numeric', $param = array())
 		{
 			array_push(self::$routes, Array(
 				'page'		=> $page,
 				'pattern'	=> $pattern,
 				'param'		=> $param,
+				'match'		=> $match,
+				'type'		=> $type,
 			));
 		}
 
@@ -48,17 +58,40 @@
 			
 			foreach (self::$routes as $route)
 			{
+				$result['page']		= $route['page'];
+				$result['param']	= $route['param'];
+				
 				foreach($route['pattern'] as $k => $v)
 				{
 					if(preg_match($v, $url_path, $matches))
 					{
-						$result['page']		= $route['page'];
-						$result['match1']	= @$matches[1];
-						$result['match2']	= @$matches[2];
-						$result['param']	= $route['param'];
-						return $result;
+						if($route['type'] == 'numeric')
+						{
+							if(is_numeric($matches[$route['match']]))
+							{
+								$result['match'] = (int) $matches[$route['match']];
+								//bir sonuç bulduysak ana döngüyü de alt döngüyü de kıralım
+								break 2;
+							}
+						}
+						
+						if($route['type'] == 'string')
+						{
+							if(is_string($matches[$route['match']]))
+							{
+								$result['match'] = (string) rawurldecode($matches[$route['match']]);
+								//bir sonuç bulduysak ana döngüyü de alt döngüyü de kıralım
+								break 2;
+							}
+						}
 					}
 				}
 			}
+			
+			if(!empty($result['match']))
+			{
+				return $result;
+			}
+			
 		}
 	}
